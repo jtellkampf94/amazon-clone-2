867226bbc373de6ae424ca603e27e8f528734000
Jonathan <jtellkampf@gmail.com>
2021-07-08 15:35:36 +0100

    Created routes and controllers for create, get, get single, update and delete products

--- server/controllers/productController.ts
+++ server/controllers/productController.ts
@@ -2,27 +2,122 @@ import { Request, Response, NextFunction } from "express";
 
 import Product from "../models/Product";
 
-// Create new product
+// Create new product => /api/v1/admin/products/new
 export const createProduct = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const product = await Product.create(req.body);
+  try {
+    const product = await Product.create(req.body);
 
-  res.status(201).json({
-    success: true,
-    product
-  });
+    res.status(201).json({
+      success: true,
+      product
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const getProducts = (
+// Get all products => /api/v1/products
+export const getProducts = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(200).json({
-    success: true,
-    message: "This route will show all products in database"
-  });
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      success: true,
+      count: products.length,
+      products
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Get single product => /api/v1/product/:id
+export const getSingleProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      product
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Update Product => /api/v1/admin/product/:id
+export const updateProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      });
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false
+    });
+
+    res.status(200).json({
+      success: true,
+      product
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Delete product => /api/v1/admin/product/:id
+export const deleteProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      });
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+      success: true,
+      message: "Product is deleted"
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- server/routes/productRoutes.ts
+++ server/routes/productRoutes.ts
@@ -1,10 +1,22 @@
 import express from "express";
 const router = express.Router();
 
-import { getProducts, createProduct } from "../controllers/productController";
+import {
+  getProducts,
+  createProduct,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct
+} from "../controllers/productController";
 
 router.route("/products").get(getProducts);
+router.route("/product/:id").get(getSingleProduct);
 
-router.route("/products/new").post(createProduct);
+router.route("/admin/products/new").post(createProduct);
+
+router
+  .route("/admin/product/:id")
+  .put(updateProduct)
+  .delete(deleteProduct);
 
 export default router;

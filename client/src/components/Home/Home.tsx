45e93fcb458785da2f410add61ec178f63b89dd6
Jonathan <jtellkampf@gmail.com>
2021-07-18 17:21:11 +0100

    created product component by separating product component from home component. Rendered product component on home component

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -3,6 +3,7 @@ import { useActions } from "../../hooks/useActions";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 
 import MetaData from "../MetaData/MetaData";
+import Product from "../Product/Product";
 
 const Home: React.FC = () => {
   const { loading, products, productsCount, errors } = useTypedSelector(
@@ -23,37 +24,7 @@ const Home: React.FC = () => {
         <div className="row">
           {products &&
             products.map(product => (
-              <div
-                key={product._id}
-                className="col-sm-12 col-md-6 col-lg-3 my-3"
-              >
-                <div className="card p-3 rounded">
-                  <img
-                    className="card-img-top mx-auto"
-                    src={product.images[0].url}
-                  />
-                  <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">
-                      <a href="">{product.name}</a>
-                    </h5>
-                    <div className="ratings mt-auto">
-                      <div className="rating-outer">
-                        <div
-                          className="rating-inner"
-                          style={{ width: `${(product.rating / 5) * 100}%` }}
-                        ></div>
-                      </div>
-                      <span id="no_of_reviews">
-                        ({product.numberOfReviews} Reviews)
-                      </span>
-                    </div>
-                    <p className="card-text">${product.price}</p>
-                    <a href="#" id="view_btn" className="btn btn-block">
-                      View Details
-                    </a>
-                  </div>
-                </div>
-              </div>
+              <Product key={product._id} product={product} />
             ))}
         </div>
       </section>
---
+++ client/src/components/Product/Product.tsx
@@ -0,0 +1,35 @@
+import { Product as ProductInterface } from "../../redux/reducers/productReducer";
+
+interface ProductProps {
+  product: ProductInterface;
+}
+
+const Product: React.FC<ProductProps> = ({ product }) => {
+  return (
+    <div key={product._id} className="col-sm-12 col-md-6 col-lg-3 my-3">
+      <div className="card p-3 rounded">
+        <img className="card-img-top mx-auto" src={product.images[0].url} />
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title">
+            <a href="">{product.name}</a>
+          </h5>
+          <div className="ratings mt-auto">
+            <div className="rating-outer">
+              <div
+                className="rating-inner"
+                style={{ width: `${(product.rating / 5) * 100}%` }}
+              ></div>
+            </div>
+            <span id="no_of_reviews">({product.numberOfReviews} Reviews)</span>
+          </div>
+          <p className="card-text">${product.price}</p>
+          <a href="#" id="view_btn" className="btn btn-block">
+            View Details
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;

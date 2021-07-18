12aed95c909073c694551f69f9bcde9da58f934b
Jonathan <jtellkampf@gmail.com>
2021-07-18 12:38:29 +0100

    Created and styled home component

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,11 +1,14 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Home from "../Home/Home";
+
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );

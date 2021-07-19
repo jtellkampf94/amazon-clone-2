a0e0f1f88075fb4de9eb53273e0e7188305856fb
Jonathan <jtellkampf@gmail.com>
2021-07-19 15:02:48 +0100

    Created product action creators

--- client/src/redux/actionCreators/index.ts
+++ client/src/redux/actionCreators/index.ts
@@ -1,1 +1,2 @@
 export * from "./productsActionCreators";
+export * from "./productActionCreators";
---
+++ client/src/redux/actionCreators/productActionCreators.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import {
+  ClearProductErrorsAction,
+  GetProductFailureAction,
+  GetProductRequestAction,
+  GetProductSuccessAction
+} from "../actions";
+
+import { ActionTypes } from "../actionTypes";
+
+export const getProduct = (id: string) => async (
+  dispatch: Dispatch
+): Promise<GetProductSuccessAction | GetProductFailureAction> => {
+  try {
+    const getProductRequestAction: GetProductRequestAction = {
+      type: ActionTypes.GET_PRODUCT_REQUEST
+    };
+    dispatch(getProductRequestAction);
+
+    const { data } = await axios.get(`/api/v1/product/${id}`);
+    const action: GetProductSuccessAction = {
+      type: ActionTypes.GET_PRODUCT_SUCCESS,
+      payload: data.product
+    };
+
+    return dispatch(action);
+  } catch (error) {
+    const action: GetProductFailureAction = {
+      type: ActionTypes.GET_PRODUCT_FAILURE,
+      payload: error.response.data.message
+    };
+    return dispatch(action);
+  }
+};
+
+export const clearProductErrors = () => (
+  dispatch: Dispatch
+): ClearProductErrorsAction => {
+  const action: ClearProductErrorsAction = { type: ActionTypes.CLEAR_ERRORS };
+  return dispatch(action);
+};
